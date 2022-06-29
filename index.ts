0885dad77b7ed9121af0450a99b982bd389d5ec0
den <denvstheworld@gmail.com>
2022-06-29 07:51:55 -0400

    Thunderstore stuff

--- index.ts
+++ index.ts
@@ -9,6 +9,35 @@ interface Config {
     outpath: string;
 }
 
+interface IManifest {
+    name: string,
+    version_number: string,
+    website_url: string,
+    description: string,
+    dependencies: string[];
+}
+
+class Manifest implements IManifest {
+    name: string;
+    version_number: string;
+    website_url: string;
+    description: string;
+    dependencies: string[];
+
+    constructor(name: string, version_number: string, website_url: string, description: string, dependencies: string[]) {
+        this.name = name;
+        this.version_number = version_number;
+        this.website_url = website_url;
+        this.description = description;
+        this.dependencies = dependencies;
+    }
+
+    toBuffer() {
+        return Buffer.from(JSON.stringify(this, null, 2));
+    }
+
+}
+
 if (!fs.existsSync("./config.json")) {
     createConfig();
 } else {
@@ -39,12 +68,20 @@ function build() {
     dist();
 }
 
+function generate_manifest() {
+    let meta: any = JSON.parse(fs.readFileSync("./package.json").toString());
+    let m = new Manifest(DLL_NAME, meta.version, meta.homepage, meta.description, meta.thunderstore.dependencies);
+    return m.toBuffer();
+}
+
 function dist() {
     // Pack dll into zip file.
     let inpath: string = path.resolve(`./bin/Release/netstandard2.1/${DLL_NAME}.dll`);
     let zip = new AdmZip();
-    zip.addFile(`${DLL_NAME}/`, Buffer.alloc(0));
-    zip.addFile(`${DLL_NAME}/${DLL_NAME}.dll`, fs.readFileSync(inpath));
+    zip.addFile(`${DLL_NAME}.dll`, fs.readFileSync(inpath));
+    zip.addFile(`manifest.json`, generate_manifest());
+    zip.addFile(`README.md`, fs.readFileSync("./README.md"));
+    zip.addFile(`icon.png`, fs.readFileSync("./icon.png"));
     if (!fs.existsSync("./dist")) {
         fs.mkdirSync("./dist");
     }
